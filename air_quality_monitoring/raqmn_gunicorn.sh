d34eafe220a549d982e6c347b39424353fb80dab
shubhamjain059 <shubhamjain0594@gmail.com>
2015-12-15 22:22:48 +0530

    Added files to run NGINX as reverse web proxy server and Gunicorn as Python WSGI HTTP Server, added instructions to help install

---
+++ air_quality_monitoring/raqmn_gunicorn.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+NAME="raqmn"                               # Name of the application
+DJANGODIR=/home/dora/Study/BTP_Bhaskaran/raqmn/utils/Real-Time-Air-Quality-Sensoring-Network/air_quality_monitoring/raqmn          # Django project directory
+BINDFILE=0.0.0.0:8000 # we will communicte using this binding
+NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=raqmn.settings             # which settings file should Django use
+DJANGO_WSGI_MODULE=raqmn.wsgi                     # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ../../../btp_venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+# RUNDIR=$(dirname $SOCKFILE)
+# test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec ../../../btp_venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --bind=$BINDFILE \
+  --log-level=debug \
+  --log-file=-
+  --reload
