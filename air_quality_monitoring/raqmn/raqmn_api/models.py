d2de590a7cae40a62c2f264f15688c9b45a1a26f
shubhamjain059 <shubhamjain0594@gmail.com>
2016-06-12 19:07:51 +0530

    Updated giles to giles2, quasar to btrdb and updated final code

--- air_quality_monitoring/raqmn/raqmn_api/models.py
+++ air_quality_monitoring/raqmn/raqmn_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class DylosData(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4(),editable=False)
+	path = models.CharField(max_length=100, unique=True)
+	
